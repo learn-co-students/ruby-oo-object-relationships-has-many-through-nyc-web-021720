099b7b3abca0e47aba5327ce55745c62e9e8a64e
CBreakr <github email address>
2020-02-24 16:46:18 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
 class Meal
 
+    attr_reader :waiter, :customer, :total, :tip
+
+    @@all
+
+    def initialize(
+        meal_waiter, 
+        meal_customer, 
+        meal_total, 
+        meal_tip
+    )
+        @waiter = meal_waiter
+        @customer = meal_customer
+        @total = meal_total
+        @tip = meal_tip
+        Meal.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end

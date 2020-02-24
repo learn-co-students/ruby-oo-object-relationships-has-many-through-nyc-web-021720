be2c184f659b2bc61690e0851b358ad1ea6f9d3c
Uriel <u.e.rodriguez@gmail.com>
2020-02-24 11:38:12 -0500

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,43 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      # selects between two elements, returning the max of the two 
+      meal_a.tip <=> meal_b.tip
+    end
+   
+    # checks the returned value's customer attribute 
+    best_tipped_meal.customer
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
 end
